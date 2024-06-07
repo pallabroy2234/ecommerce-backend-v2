d574dbca296f86f21503c0128b565293255d3c3e
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-06-07 21:06:19 +0600

    feat: handleGetAllUsers and handleGetUser

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -2,6 +2,7 @@ import {Request, Response, NextFunction} from "express";
 import UserModal from "../models/userModal.js";
 import {NewUserRequestBody} from "../types/types.js";
 import {TryCatch} from "../middlewares/error.js";
+import ErrorHandler from "../utils/utility-class.js";
 
 // * handleNewUser -> /api/v1/user/new
 export const handleNewUser = TryCatch(async (req: Request<{}, {}, NewUserRequestBody>, res: Response, next: NextFunction) => {
@@ -27,6 +28,26 @@ export const handleNewUser = TryCatch(async (req: Request<{}, {}, NewUserRequest
 	});
 	return res.status(201).json({
 		success: true,
-		message: `Welcome ${name}! Your account has been created successfully!`,
+		message: `Welcome ${user.name}! Your account has been created successfully!`,
+	});
+});
+
+export const handleGetAllUsers = TryCatch(async (req, res, next) => {
+	const users = await UserModal.find({});
+	return res.status(200).json({
+		success: true,
+		payload: users,
+	});
+});
+
+export const handleGetUser = TryCatch(async (req, res, next) => {
+	const {id} = req.params;
+
+	const user = await UserModal.findById({_id: id});
+	console.log(user);
+	if (!user) return next(new ErrorHandler("Invalid user id", 400));
+	return res.status(200).json({
+		success: true,
+		payload: user,
 	});
 });
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,11 +1,17 @@
 import express from "express";
 
 const userRouter = express.Router();
-import {handleNewUser} from "../controllers/userControllers.js";
+import {handleGetAllUsers, handleGetUser, handleNewUser} from "../controllers/userControllers.js";
 import {validateUser} from "../validators/validateUser.js";
 import {runValidation} from "../validators/index.js";
 
 // * Route -> /api/v1/user/new
 userRouter.post("/new", validateUser, runValidation(), handleNewUser);
+
+// * Route -> /api/v1/user/all
+userRouter.get("/all", handleGetAllUsers);
+
+// * Route -> /api/v1/user/:id
+userRouter.get("/:id", handleGetUser);
 
 export default userRouter;
