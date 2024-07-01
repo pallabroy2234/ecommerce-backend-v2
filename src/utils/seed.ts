1b991c66f90988f67070572d916c30d1d703c350
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-07-01 13:40:03 +0600

    feat: create a seeding utility for seeding data into database for development purpose

---
+++ src/utils/seed.ts
@@ -0,0 +1,20 @@
+import {faker} from "@faker-js/faker";
+import {Product} from "../models/productModel.js";
+// * Function to seed products
+const seedProducts = async (count: number = 10) => {
+	const products = [];
+	for (let i = 0; i < count; i++) {
+		const product = {
+			name: faker.commerce.productName(),
+			image: "public\\\\uploads\\\\cb69ca8f-156c-4d87-af24-a8191ccd72a9.png",
+			price: faker.commerce.price({min: 1500, max: 50000, dec: 0}),
+			stock: faker.commerce.price({min: 0, max: 200, dec: 0}),
+			category: faker.commerce.department(),
+		};
+		products.push(product);
+	}
+	await Product.create(products);
+	console.log("Products seeded successfully");
+};
+
+// seedProducts(100);
