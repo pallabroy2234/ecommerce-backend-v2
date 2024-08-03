9236e0e7188a0f450124112fe1dfe3a842a1cf8e
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-08-04 00:31:05 +0600

    refactor: refactor validatePayment file

--- src/validators/validatePayemnt.ts
+++ src/validators/validatePayemnt.ts
@@ -1,5 +1,26 @@
 import {body, param, query} from "express-validator";
 
+/**
+ * @description Validates the body of the Create New Coupon endpoint.
+ *
+ * @route      POST /api/v1/payment/coupon
+ * @access     Private/Admin
+ *
+ * @validation
+ *  - "coupon":
+ *    - Required, must be a string with only letters and numbers.
+ *  - "amount":
+ *    - Required, must be a positive number.
+ *
+ * @errors
+ *  - "Please enter a coupon code"
+ *  - "Coupon code must be a string"
+ *  - "Coupon code can only contain letters and numbers"
+ *  - "Please enter a discount amount"
+ *  - "Invalid discount amount. Please enter a valid amount"
+ *  - "Discount amount must be greater than zero"
+ */
+
 export const validateNewCoupon = [
 	body("coupon")
 		.notEmpty()
@@ -23,6 +44,22 @@ export const validateNewCoupon = [
 		}),
 ];
 
+/**
+ * @description       Middleware for validation the parameters of the Apply Coupon API endpoint.
+ * @route             GET /api/v1/payment/coupon/discount?coupon=COUPON
+ * @access            Private/ Only for user
+ *
+ * @validation
+ *  - query("coupon"): Coupon Code
+ *  - Must be not empty
+ *  - Must be a string
+ *  - Must match the regex /^[A-Za-z0-9]+$/
+ *  - Must be a valid coupon code
+ *
+ * @errors
+ *  - "Invalid Coupon Code": Returned if the coupon code is missing, not a string, or contains special characters.
+ * */
+
 export const validateApplyCouponCode = [
 	query("coupon")
 		.notEmpty()
@@ -32,7 +69,7 @@ export const validateApplyCouponCode = [
 ];
 
 /**
- * @desc          Delete Coupon params validation
+ * @description   Middleware for validating the parameters of the Delete Coupon API endpoint.
  * @route         DELETE /api/v1/payment/coupon/:id
  * @access        Private/Admin
  *
