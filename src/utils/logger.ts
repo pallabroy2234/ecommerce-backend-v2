1e8bb4cc8851b7b60da1238fda131aed72fa0545
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-09-10 04:32:38 +0600

    feat: change tsconfig.json file

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,60 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
+// dotenv.config();
+// import DailyRotateFile from "winston-daily-rotate-file";
+// import path from "path";
+// import {createLogger, format, transports} from "winston";
+// import {getDirname} from "./getDirname.js";
+
+// const {combine, timestamp, printf, errors, colorize, json} = format;
+//
+// const __dirname = getDirname(import.meta.url);
+//
+// const logFormat = printf(({level, message, timestamp, stack}) => {
+// 	return `${timestamp} ${level}: ${stack || message}`;
+// });
+//
+// const logger = createLogger({
+// 	level: "info",
+// 	format: combine(timestamp({format: "YYYY-MM-DD HH:mm:ss"}), errors({stack: true}), logFormat),
+// 	transports: [
+// 		// * This transport for duplicate logs in console so commented
+// 		// new transports.Console({
+// 		// 	format: combine(colorize(), logFormat),
+// 		// }),
+// 		new DailyRotateFile({
+// 			dirname: path.join(__dirname, "../../src/logs"),
+// 			filename: "app-%DATE%.log",
+// 			// datePattern: "YYYY-MM-DD-HH-mm",
+// 			datePattern: "YYYY-MM-DD",
+// 			// maxSize: "20m",
+// 			maxSize: "5m", // * "5m" = 5mb, "5k" = 5kb, "5g" = 5gb, "5t" = 5tb, "5b" = 5 bytes
+// 			maxFiles: "5d",
+// 			zippedArchive: true,
+// 			format: combine(json()),
+// 		}),
+// 	],
+// 	exceptionHandlers: [
+// 		new transports.File({
+// 			dirname: path.join(__dirname, "../../src/logs"),
+// 			filename: "exceptions.log",
+// 		}),
+// 	],
+// 	rejectionHandlers: [
+// 		new transports.File({
+// 			dirname: path.join(__dirname, "../../src/logs"),
+// 			filename: "rejections.log",
+// 		}),
+// 	],
+// });
+//
+// logger.add(
+// 	new transports.Console({
+// 		format: combine(colorize(), logFormat),
+// 	}),
+// );
+
+import dotenv from "dotenv";
 dotenv.config();
 import DailyRotateFile from "winston-daily-rotate-file";
 import path from "path";
@@ -8,27 +63,24 @@ import {getDirname} from "./getDirname.js";
 
 const {combine, timestamp, printf, errors, colorize, json} = format;
 
-const __dirname = getDirname(import.meta.url);
+// Define __dirname for ES modules
+// const __dirname = getDirname(import.meta.url);
 
 const logFormat = printf(({level, message, timestamp, stack}) => {
 	return `${timestamp} ${level}: ${stack || message}`;
 });
 
+const logDir = "/tmp/logs"; // Update this as needed
+
 const logger = createLogger({
 	level: "info",
 	format: combine(timestamp({format: "YYYY-MM-DD HH:mm:ss"}), errors({stack: true}), logFormat),
 	transports: [
-		// * This transport for duplicate logs in console so commented
-		// new transports.Console({
-		// 	format: combine(colorize(), logFormat),
-		// }),
 		new DailyRotateFile({
-			dirname: path.join(__dirname, "../../src/logs"),
+			dirname: logDir,
 			filename: "app-%DATE%.log",
-			// datePattern: "YYYY-MM-DD-HH-mm",
 			datePattern: "YYYY-MM-DD",
-			// maxSize: "20m",
-			maxSize: "5m", // * "5m" = 5mb, "5k" = 5kb, "5g" = 5gb, "5t" = 5tb, "5b" = 5 bytes
+			maxSize: "5m",
 			maxFiles: "5d",
 			zippedArchive: true,
 			format: combine(json()),
@@ -36,13 +88,13 @@ const logger = createLogger({
 	],
 	exceptionHandlers: [
 		new transports.File({
-			dirname: path.join(__dirname, "../../src/logs"),
+			dirname: logDir,
 			filename: "exceptions.log",
 		}),
 	],
 	rejectionHandlers: [
 		new transports.File({
-			dirname: path.join(__dirname, "../../src/logs"),
+			dirname: logDir,
 			filename: "rejections.log",
 		}),
 	],
@@ -54,6 +106,8 @@ logger.add(
 	}),
 );
 
+export default logger;
+
 // * This is only logger for development mode to show logs in console
 // if (process.env.MODE !== "production") {
 // 	logger.add(
@@ -62,5 +116,3 @@ logger.add(
 // 		}),
 // 	);
 // }
-
-export default logger;
